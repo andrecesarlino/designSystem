3b907850febf7dedcb74b3600211949b90ed6016
Andre Lino <andre.cesar97@hotmail.com>
2023-03-15 10:58:30 -0300

    ajustes do designsystem

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Enviar',
+  },
+}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Fechar',
+    size: 'big',
+  },
+}
