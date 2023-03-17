52b959b6e9b50fb2d626d7295ae69eee4a83fd58
Andre Lino <andre.cesar97@hotmail.com>
2023-03-17 16:15:22 -0300

    ajustes de controle

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,30 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora quo tenetur vero, impedit est nostrum delectus, hic earum maiores sit repellendus voluptatum ullam ut magni in quidem nam quam! Facilis?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
